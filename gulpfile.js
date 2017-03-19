fff5b6ecd18651d8831a3fc3a529f272464b8dde
Yaw Boakye <wheresyaw@gmail.com>
2017-03-19 09:32:51 +0000

    beef up gulp build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+const gulp        = require('gulp');
+const sass        = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const useref      = require('gulp-useref');
+const uglify      = require('gulp-uglify');
+const gulpIf      = require('gulp-if');
+const cssnano     = require('gulp-cssnano');
+const imagemin    = require('gulp-imagemin');
+const cache       = require('gulp-cache');
+const del         = require('del');
+const runSequence = require('run-sequence');
 
 /* BROWSERSYNC TASK */
 gulp.task('browserSync', function() {
@@ -21,10 +29,54 @@ gulp.task('sass', function() {
         }));
 });
 
+/* USEREF TASK */
+gulp.task('useref', function(){
+    return gulp.src('app/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+/* IMAGES TASK */
+gulp.task('images', function(){
+    return gulp.src('app/resources/images/*.+(png|jpg|gif|svg)')
+    .pipe(cache(imagemin({
+        interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'));
+});
+
+/* FONTS TASK */
+gulp.task('fonts', function() {
+    return gulp.src('app/vendors/fonts/*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+/* Clean up task */
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+});
+
 
 /* Gulp watch file - watches for changes to any file */
 gulp.task('watch', ['browserSync', 'sass'], function() {
     gulp.watch('app/scss/**/*.scss', ['sass']);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+/* Build tasks */
+gulp.task('default', function(callback) {
+    runSequence(['sass', 'browserSync'], 'watch',
+    callback
+    );
+});
+
+gulp.task('build', function(callback) {
+    runSequence('clean:dist','sass', ['useref', 'images', 'fonts'],
+    callback
+    );
 });
